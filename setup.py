61eea3e755e60381da3e20397295126a5488ffad
Divyansh Pandey <113619486+Divyansh0108@users.noreply.github.com>
2024-09-09 21:49:28 +0530

    setup the environment

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+from typing import List
+
+HYPHEN_E_DOT = "-e ."
+
+def get_requirements(file_path: str) -> List[str]:
+    '''
+    This function reads the requirements file and returns a list of requirements.
+    '''
+    requirements = []
+    with open(file_path, 'r') as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [req.replace('\n', '') for req in requirements]
+
+        if HYPHEN_E_DOT in requirements:
+            requirements.remove(HYPHEN_E_DOT)
+
+    return requirements
+
+setup(
+    name="ML Project",
+    version="0.1",
+    author="Divyansh",
+    author_email="divyanshpandey0108@gmail.com",
+    packages=find_packages(),
+    install_requires=get_requirements('requirements.txt'),
+)
